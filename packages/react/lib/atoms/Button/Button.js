19e01eeacdb6513d513ec97c597851218f33856a
Md. Abu Sayeed Mondal <sayeedmondal1412@gmail.com>
2021-11-07 15:26:22 +0600

    added rollup

---
+++ packages/react/lib/atoms/Button/Button.js
@@ -0,0 +1,8 @@
+import { jsx } from 'react/jsx-runtime';
+
+const Button = ({ label }) => {
+    return jsx("button", { children: label }, void 0);
+};
+
+export { Button as default };
+//# sourceMappingURL=Button.js.map
